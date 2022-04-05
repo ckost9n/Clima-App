0bcc5d516f0c65f0971dc2aa2d76e0d4e1d0b28c
ckost9n <ckost9n@gmail.com>
2022-04-05 11:35:19 +0300

    first UI setup

---
+++ Clima App/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherViewController.swift
+//  Clima App
+//
+//  Created by Konstantin on 05.04.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet var coditionImageView: UIImageView!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
