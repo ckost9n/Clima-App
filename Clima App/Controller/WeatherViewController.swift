5bc81024aefc61fef6182a2878bebc18b5e1fd1d
ckost9n <ckost9n@gmail.com>
2022-04-05 13:36:55 +0300

    add textFieldDelegate

--- Clima App/Controller/WeatherViewController.swift
+++ Clima App/Controller/WeatherViewController.swift
@@ -12,12 +12,46 @@ class WeatherViewController: UIViewController {
     @IBOutlet var coditionImageView: UIImageView!
     @IBOutlet var temperatureLabel: UILabel!
     @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var searchTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
+        
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? #function)
+    }
+    
 }
 
+// MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text ?? #function)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if !textField.text!.isEmpty {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        
+        
+        
+        searchTextField.text = ""
+    }
+
+}
